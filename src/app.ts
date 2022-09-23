452c67dc1493afcd0d52342c0fc08863e000ecb0
Elias Merhrioui <e.merhrioui@gmail.com>
2022-09-23 18:04:29 +0200

    bug prisma bdd

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route";
+
+const server = Fastify();
+
+server.get('/healthcheck', async function() {
+    return { status: "OK" };
+})
+
+
+async function main() {
+    server.register(userRoutes, {prefix: "/users"})
+    
+    
+    
+    try {
+        await server.listen(3000, "0.0.0.0");
+
+        console.log('Serveur prêt : http://localhost:3000');
+    } catch(e){
+        console.error(e);
+        process.exit(1);
+    }
+}    
+   
+main();
